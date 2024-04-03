1a6523f2c5356196af10107f30a98999bd21ca2a
Ahmad Jawad <134083952+ahmedjawad1857@users.noreply.github.com>
2024-04-03 15:41:51 +0500

    project-11 my-bank-OOP maked by using inquirer,typeScript

---
+++ TS-Q1/node-projects/project_11_oop_mybank/Readme.md
@@ -0,0 +1,44 @@
+# m-my-bank-app
+
+m-my-bank-app is a simple command-line interface application that allows users to manage their bank account transactions.
+
+## Installation
+
+To install m-my-bank-app, use npm:
+
+```bash
+npm install -g m-my-bank-app
+```
+
+## Usage
+
+Once installed, you can run m-my-bank-app by typing `mybank` in your terminal.
+
+```bash
+m-my-bank-app
+```
+
+## Use without installing
+
+```bash
+npx m-my-bank-app
+```
+
+Follow the on-screen prompts to provide your personal information and perform transactions.
+
+### Options
+
+- **Debit**: Deducts a specified amount from your account balance.
+- **Credit**: Adds a specified amount to your account balance.
+- **Exit**: Exits the application.
+
+## Features
+
+- Interactive command-line interface.
+- Ability to perform debit and credit transactions.
+- Input validation for transaction amounts and user age.
+- Continuous operation until the user chooses to exit.
+
+## Author
+
+This project is created by **_M.Ahmad jawad_**
---
+++ TS-Q1/node-projects/project_11_oop_mybank/index.ts
@@ -0,0 +1,155 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+interface BankAccountType {
+  Debit(d: number): string;
+  Credit(d: number): string;
+}
+
+class BankAccount implements BankAccountType {
+  accountBalance: number;
+
+  constructor() {
+    this.accountBalance = 100;
+  }
+
+  Credit(amount: number): string {
+    let statement: string = "Somehting Went Wrong!";
+    if (amount > 0) {
+      this.accountBalance += amount;
+      if (amount > 100) {
+        this.accountBalance -= 1;
+      }
+      statement = `Your account has been credited successfully! and now your balance is ${this.accountBalance}`;
+    }
+    return statement;
+  }
+
+  Debit(amount: number): string {
+    let statement: string = "Something Went Wrong!";
+    if (amount > 0) {
+      if (this.accountBalance >= amount) {
+        this.accountBalance -= amount;
+        statement = `Transaction Successful! Now account balance is ${this.accountBalance}`;
+      } else {
+        statement = "Sorry, you have insufficient balance.";
+      }
+    }
+    return statement;
+  }
+}
+
+class Customer {
+  firstName: string;
+  lastName: string;
+  gender: string;
+  age: number;
+  mobileNumber: string;
+  bankAccount: BankAccount;
+
+  constructor() {
+    this.firstName = "";
+    this.lastName = "";
+    this.gender = "";
+    this.age = 0;
+    this.mobileNumber = "";
+    this.bankAccount = new BankAccount();
+  }
+
+  async getCustomerInfo() {
+    const answers = await inquirer.prompt([
+      {
+        type: "input",
+        name: "firstName",
+        message: "Enter your first name:",
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "Enter your last name:",
+      },
+      {
+        type: "list",
+        name: "gender",
+        message: "Enter your gender:",
+        choices: ["Male", "Female", "Rather Not Say"],
+      },
+      {
+        type: "number",
+        name: "age",
+        message: "Enter your age(must greater than 18.):",
+        validate: function (value) {
+          const valid = !isNaN(parseFloat(value)) && value >= 18;
+          return (
+            valid || "Please enter your age and age must be greater than 18"
+          );
+        },
+      },
+      {
+        type: "input",
+        name: "mobileNumber",
+        message: "Enter your mobile number:",
+      },
+    ]);
+
+    this.firstName = answers.firstName;
+    this.lastName = answers.lastName;
+    this.gender = answers.gender;
+    this.age = answers.age;
+    this.mobileNumber = answers.mobileNumber;
+  }
+
+  customerInfo(): string {
+    return `
+    Name: ${this.firstName} ${this.lastName}
+    Age: ${this.age}
+    Gender: ${this.gender}
+    Mobile Number: ${this.mobileNumber}
+    Account Balance: ${this.bankAccount.accountBalance}
+    `;
+  }
+
+  async performTransaction() {
+    const transactionType = await inquirer.prompt({
+      type: "list",
+      name: "transactionType",
+      message: "Choose transaction type:",
+      choices: ["Debit", "Credit", "Exit"],
+    });
+
+    if (transactionType.transactionType === "Exit") {
+      console.log("Exiting program...");
+      process.exit(0);
+    }
+
+    const amountAnswer = await inquirer.prompt({
+      type: "number",
+      name: "amount",
+      message: `Enter amount to ${transactionType.transactionType}:`,
+      validate: function (value) {
+        const valid = !isNaN(parseFloat(value));
+        return valid || "Please enter a valid number";
+      },
+    });
+
+    const amount = amountAnswer.amount;
+    if (isNaN(amount)) {
+      console.log("Invalid amount. Please enter a number.");
+      return;
+    }
+
+    if (transactionType.transactionType === "Debit") {
+      console.log(this.bankAccount.Debit(amount));
+    } else if (transactionType.transactionType === "Credit") {
+      console.log(this.bankAccount.Credit(amount));
+    }
+  }
+}
+
+const customer = new Customer();
+await customer.getCustomerInfo();
+console.log(customer.customerInfo());
+
+while (true) {
+  await customer.performTransaction();
+}
